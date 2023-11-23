3fc6be7cfcd4bb724b5e88fb43fa6955227da42c
George Bacon <george.bacon@live.co.uk>
2023-11-23 10:23:13 +0000

    finished game

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,42 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (player, computer, target) => {
+  if (player < 0 || player > 9) {
+    return alert("invalid input");
+  }
+
+  let playerDelta = getAbsoluteDistance(player, target);
+  let computerDelta = getAbsoluteDistance(computer, target);
+
+  //   playerDelta = Math.abs(playerDelta);
+  //   computerDelta = Math.abs(computerDelta);
+
+  if (playerDelta === computerDelta) {
+    return true;
+  } else if (playerDelta < computerDelta) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else if (winner === "computer") {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
+
+const getAbsoluteDistance = (guess, target) => {
+  return Math.abs(guess - target);
+};
